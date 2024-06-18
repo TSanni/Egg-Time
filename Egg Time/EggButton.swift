48d67be587c57657ea19f16bfc9fd33d434917af
Tomas Sanni <tomas.sanni7@gmail.com>
2024-06-18 05:39:05 -0500

    Complete

---
+++ Egg Time/EggButton.swift
@@ -0,0 +1,20 @@
+//
+//  EggButton.swift
+//  Egg Time
+//
+//  Created by Tomas Sanni on 6/17/24.
+//
+
+import UIKit
+
+class EggButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
