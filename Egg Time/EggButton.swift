61b562441fc77efb18cacab771e43c0954010108
Tomas Sanni <tomas.sanni7@gmail.com>
2024-06-18 05:40:06 -0500

    complete

--- Egg Time/EggButton.swift
+++ Egg Time/EggButton.swift
@@ -9,12 +9,52 @@ import UIKit
 
 class EggButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    var eggType: EggType = .soft
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(eggType: EggType) {
+        super.init(frame: .zero)
+        self.eggType = eggType
+        setImage(eggType.image, for: .normal)
+        setTitle(eggType.title, for: .normal)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
 
+
+enum EggType {
+    case soft
+    case medium
+    case hard
+    
+    var title: String {
+        switch self {
+        case .soft: "Soft"
+        case .medium: "Medium"
+        case .hard: "Hard"
+        }
+    }
+    
+    var image: UIImage {
+        switch self {
+        case .soft: .softEgg
+        case .medium: .mediumEgg
+        case .hard: .hardEgg
+        }
+    }
+    
+    var time: Int {
+        switch self {
+        case .soft: 3
+        case .medium: 5
+        case .hard: 7
+        }
+    }
 }
--- Egg Time/ViewController.swift
+++ Egg Time/ViewController.swift
@@ -8,12 +8,112 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let softEggImageButton = EggButton(eggType: .soft)
+    let mediumEggImageButton = EggButton(eggType: .medium)
+    let hardEggImageButton = EggButton(eggType: .hard)
+    let horizontalStack = UIStackView()
+    let progressBar = UIProgressView()
+    let titleTop = UILabel()
+    
+    var totalTime = 0
+    var secondsPassed = 0
+    var timer = Timer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = #colorLiteral(red: 0.7960784314, green: 0.9490196078, blue: 0.9882352941, alpha: 1)
+        titleTop.text = "How do you like your eggs?"
+        titleTop.font = UIFont.preferredFont(forTextStyle: .title1)
+        configureStack()
+        configureEggButtons()
+        configureProgressBar()
     }
+    
+    @objc func handleButton(button: EggButton) {
+        timer.invalidate()
+        totalTime = button.eggType.time
+        progressBar.setProgress(0.0, animated: false)
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleTop.text = button.eggType.title
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer()
+    {
+        if secondsPassed < totalTime
+        {
+            secondsPassed += 1
+            let setProgressValue = Float(secondsPassed) / Float(totalTime)
+            progressBar.setProgress(setProgressValue, animated: true)
+        }
+        else
+        {
+            timer.invalidate()
+            titleTop.text = "Done"
+        }
+    }
+    
+    func configureStack() {
+        view.addSubview(titleTop)
+        titleTop.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(horizontalStack)
+        horizontalStack.translatesAutoresizingMaskIntoConstraints = false
+        horizontalStack.axis = .horizontal
+        horizontalStack.distribution = .equalSpacing
+        horizontalStack.addArrangedSubview(softEggImageButton)
+        horizontalStack.addArrangedSubview(mediumEggImageButton)
+        horizontalStack.addArrangedSubview(hardEggImageButton)
+
+        NSLayoutConstraint.activate([
+            
+            titleTop.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            titleTop.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+
+            horizontalStack.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            horizontalStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            horizontalStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+
+        ])
+    }
+    
+    func configureEggButtons() {
+        softEggImageButton.addTarget(self, action: #selector(handleButton), for: .touchUpInside)
+        mediumEggImageButton.addTarget(self, action: #selector(handleButton), for: .touchUpInside)
+        hardEggImageButton.addTarget(self, action: #selector(handleButton), for: .touchUpInside)
+        
+        NSLayoutConstraint.activate([
+            softEggImageButton.heightAnchor.constraint(equalToConstant: 120),
+            softEggImageButton.widthAnchor.constraint(equalTo: softEggImageButton.heightAnchor, multiplier: 1),
+            
+            mediumEggImageButton.heightAnchor.constraint(equalToConstant: 120),
+            mediumEggImageButton.widthAnchor.constraint(equalTo: mediumEggImageButton.heightAnchor, multiplier: 1),
+            
+            hardEggImageButton.heightAnchor.constraint(equalToConstant: 120),
+            hardEggImageButton.widthAnchor.constraint(equalTo: hardEggImageButton.heightAnchor, multiplier: 1),
+            
+        ])
+    }
+    
+    func configureProgressBar() {
+        view.addSubview(progressBar)
+        progressBar.translatesAutoresizingMaskIntoConstraints = false
+        progressBar.progressViewStyle = .bar
+        progressBar.progressTintColor = .systemYellow
+        progressBar.setProgress(0.5, animated: true)
+        
+        NSLayoutConstraint.activate([
+            progressBar.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100),
+            progressBar.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            progressBar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            progressBar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+        ])
+    }
+}
 
 
+#Preview {
+    ViewController()
 }
 
